2d48907bbb783926845d6e34cd0cb7df2e01c26b
nryabkov <78977205+nryabkovv@users.noreply.github.com>
2023-03-25 16:41:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,31 +1,29 @@
-int countPairs1(int* arr, int len, int value)
-{
+// Copyright 2021 NNTU-CS
+int countPairs1(int* arr, int len, int value) {
     int k = 0;
     for (int i = 0; i < len - 1; i++) {
-	    for (int j = i + 1; j < len; j++) {
+        for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 k += 1;
             }
-		}
-	}
+        }
+    }
     return k;
 }
 
-int countPairs2(int* arr, int len, int value)
-{
+int countPairs2(int* arr, int len, int value) {
     int k = 0;
     for (int i = 0; i < len; ++i) {
-	    for (int j = len - 1; i < j; j--) {
+        for (int j = len - 1; i < j; j--) {
             if (arr[i] + arr[j] == value) {
                 k += 1;
             }
-		}
-	}
+        }
+    }
     return k;
 }
 
-int countPairs3(int* arr, int len, int value)
-{
+int countPairs3(int* arr, int len, int value) {
     int k = 0;
     for (int i = 0; i < len - 1; i++) {
         int left = i;
